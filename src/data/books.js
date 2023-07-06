1a7d66a0aa13a8f8d725348e9f13f8db2476cb26
YoussefAyachi95 <youssef.ayachi@web.de>
2023-07-06 14:35:26 +0200

    Add book and 404 page

---
+++ src/data/books.js
@@ -0,0 +1,49 @@
+export const books = [
+    {
+        id: 'clean-code',
+        title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+        img: 'https://m.media-amazon.com/images/I/41xShlnTZTL._AC_UF894,1000_QL80_.jpg',
+        opinion: 'While "Clean Architecture" focuses on designing software systems and their overall structure, "Clean Code" dives into the details of writing clean and readable code. Both books complement each other exceptionally well imho',
+        author: 'Robert C. Martin'
+    },
+
+    {
+        id: 'pragmatic-programmer',
+        title: 'Pragmatic Programmer, The: Your journey to mastery, 20th Anniversary Edition',
+        img: 'https://m.media-amazon.com/images/I/71f743sOPoL._SL1500_.jpg',
+        opinion: 'A great and enjoyable read. I really loved reading this book. Gave me a wider perspective of coding and software development. Currently learning Frontend Development but this book showed me how wide programming really is. The concepts in the book might be from the past but they are applicable timelessly. If you are coding this book must be on your shelf.',
+        author: 'David Thomas and Andrew Hunt'
+    },
+
+    {
+        id: 'c++-high-performance',
+        title: 'C++ High Performance: Master the art of optimizing the functioning of your C++ code, 2nd Edition',
+        img: 'https://m.media-amazon.com/images/I/717h6ceqKJL._SL1360_.jpg',
+        opinion: 'This book was a great companion in the process of learning C++ around half a year in the process. It covers many need-to-know subjects and intricacies about the language in the first many chapters. In the late chapters more advanced features are described. I like it.',
+        author: 'Bjorn Andrist and Viktor Sehr'
+    },
+
+    {
+        id: 'The-Art-of-Writing-Efficient',
+        title: 'The Art of Writing Efficient Programs: An advanced programmers guide to efficient hardware utilization and compiler optimizations using C++ examples',
+        img: 'https://m.media-amazon.com/images/I/71As4VWiOWL._SL1360_.jpg',
+        opinion: 'The need for performance is back: While performance was not the most important topic during the crazy clockspeed improvements in the 90s and 2000s, today performance again plays a major role in software development. Whether it is to reduce costs when computing something in the cloud or to increase the duration of your battery, performance is important again, and in particular in C++.',
+        author: 'Fedor G. Pikus'
+    },
+    
+    {
+        id: 'A-Philosophy-of-Software-Design',
+        title: 'A Philosophy of Software Design, 2nd Edition',
+        img: 'https://m.media-amazon.com/images/I/711Qnv05eTL._SL1231_.jpg',
+        opinion: 'Amazing book! The author combined many things that been in industry for a long and put it into a properly structured way, with a good reasoning about why something was like that and what we probably should do to avoid common mistakes, etc. You can read the whole book in a day or two yet the value you get from it is completely priceless! 10/10',
+        author: 'John Ousterhout'
+    },
+
+    {
+        id: 'Designing-Data-Intensive-Applications',
+        title: 'Designing Data-Intensive Applications: The Big Ideas Behind Reliable, Scalable, and Maintainable Systems',
+        img: 'https://m.media-amazon.com/images/I/91YfNb49PLL._AC_SL1500_.jpg',
+        opinion: 'This is one of the best technical books I have ever read. Not only is it comprehensive and thorough, but also comprehensible. Martin Kleppmann has a knack for explaining things in a manner that is easy to understand and follow, even if their complexity is non-trivial.',
+        author: 'Martin Kleppmann'
+    }
+]
